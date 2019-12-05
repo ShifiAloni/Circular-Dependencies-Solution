7b70d03446a08078b0f957a172d5c92f8a8d24a9
shifra_al <shifiredner@gmail.com>
2019-12-05 09:30:55 +0200

    first commit

---
+++ Circular-Dependencies-Solution/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Dependencies_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        #region ********** first solution - interface **********
+        /********** first solution - interface **********/
+        public class A
+        {
+            IB classB { get; set; }
+
+            public A()
+            {
+                classB.MakeCoffe();
+            }
+
+            public void CallFriends() { }
+        }
+
+        public class B : IB
+        {
+            A classA { get; set; }
+
+            public void MakeCoffe()
+            {
+                classA.CallFriends();
+            }
+        }
+
+        public interface IB
+        {
+            void MakeCoffe();
+        }
+        #endregion
+
+        #region ********** second solution - factory pattern **********
+        /********** second solution - factory pattern **********/
+        /*public class A
+        {
+            private IB classB;
+
+            public A()
+            {
+                Factory factory = new Factory();
+                classB = factory.GetBObject();
+            }
+
+            public void CallFriends() { }
+        }
+
+        public class B : IB
+        {
+            A classA { get; set; }
+
+            public void MakeCoffe()
+            {
+                classA.CallFriends();
+            }
+        }
+
+        public interface IB
+        {
+            void MakeCoffe();
+        }
+
+        public class Factory
+        {
+            public B GetBObject()
+            {
+                return new B();
+            }
+        }*/
+        #endregion
+
+    }
+}
